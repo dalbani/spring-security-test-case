e40ee2262e84391a4855946a75c0c479e9d6b8ed
Damiano Albani <damiano.albani@gmail.com>
2024-09-02 13:34:46 +0200

    Add alternate token exchange provider for comparison

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
@@ -24,15 +26,19 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedCli
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.function.Function;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -64,7 +70,7 @@ public class Config {
         };
     }
 
-    @Bean
+    // @Bean
     public OAuth2AuthorizedClientProvider tokenExchangeOAuth2AuthorizedClientProvider() {
         TokenExchangeGrantRequestEntityConverter requestEntityConverter = new TokenExchangeGrantRequestEntityConverter();
         requestEntityConverter.addParametersConverter(grantRequest -> {
@@ -92,6 +98,41 @@ public class Config {
             }
             return null;
         });
+
+        return authorizedClientProvider;
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientProvider alternateTokenExchangeOAuth2AuthorizedClientProvider(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        // This OAuth2AuthorizedClientManager is used for resolving the current
+        // user's access token.
+        OAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        Function<OAuth2AuthorizationContext, OAuth2Token> subjectResolver = (context) -> {
+            if (context.getPrincipal() instanceof OAuth2AuthenticationToken oauthAuthenticationToken) {
+                // Get the current user's client registration id
+                String clientRegistrationId = oauthAuthenticationToken.getAuthorizedClientRegistrationId();
+
+                OAuth2AuthorizeRequest authorizeRequest =
+                        OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
+                                .principal(context.getPrincipal())
+                                .build();
+                OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+                Assert.notNull(authorizedClient, "authorizedClient cannot be null");
+
+                return authorizedClient.getAccessToken();
+            }
+
+            return null; // This should probably throw an exception
+        };
+
+        TokenExchangeOAuth2AuthorizedClientProvider authorizedClientProvider =
+                new TokenExchangeOAuth2AuthorizedClientProvider();
+        authorizedClientProvider.setSubjectTokenResolver(subjectResolver);
 
         return authorizedClientProvider;
     }
